5b502725dbd967c26815b607ee17dde7c6178b0e
javlonrahmatullayev07 <javlonrahmatullayev07@gmail.com>
2023-11-08 08:50:09 +0500

    CODE: RUB: Fix code of header.js

--- js/header.js
+++ js/header.js
@@ -17,18 +17,31 @@ overlay.addEventListener("click", function () {
 // dropdown
 const hasSubMenuElements = document.querySelectorAll(".has_sub_menu");
 
-const mainElement = document.querySelector("main");
-
 hasSubMenuElements.forEach(function (element) {
+  element.addEventListener("click", function (event) {
+    event.stopPropagation(); // Prevent the click event from propagating to parent elements
+    const subMenu = this.nextElementSibling;
+    
+    // Check if the submenu is already visible
+    if (subMenu.style.display === "block") {
+      subMenu.style.display = "none"; // Hide the submenu if it's already visible
+    } else {
+      // Hide all other submenus
+      const otherSubMenus = document.querySelectorAll(".sub_menu:not(.active)");
+      otherSubMenus.forEach(function (subMenu) {
+        subMenu.style.display = "none";
+      });
+      
+      subMenu.style.display = "block"; // Show the clicked submenu
+    }
+  });
+
   element.addEventListener("mouseover", function () {
-    const otherSubMenus = document.querySelectorAll(".sub_menu:not(.active)");
-    otherSubMenus.forEach(function (subMenu) {
-      subMenu.style.display = "none";
-    });
     const subMenu = this.nextElementSibling;
     subMenu.style.display = "block";
   });
 });
+
 
 mainElement.addEventListener("mouseover", function () {
   const subMenus = document.querySelectorAll(".sub_menu");
